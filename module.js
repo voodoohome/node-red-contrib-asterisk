d83648ab13f683a70fdfcd9f356b91a42f88f7f5
Stefan Koch <stefkoch@de.ibm.com>
2016-04-16 00:15:08 +0200

    npm bootstrap

--- module.js
+++ module.js
@@ -1,3 +1,30 @@
 /**
  * This is a test
  **/
+
+
+module.exports = function(RED) {
+
+    function AsteriskControllerNode(config) {
+        RED.nodes.createNode(this,config);
+
+    }
+    RED.nodes.registerType("asterisk-controller",AsteriskControllerNode);
+
+
+    function AsteriskInputNode(config) {
+        RED.nodes.createNode(this,config);
+
+        this.on("input", function(msg) {
+            if (msg != null) {
+              console.log(msg);
+            };
+        });
+
+    }
+    RED.nodes.registerType("asterisk-in",AsteriskInputNode);
+
+
+
+
+}
