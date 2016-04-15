33290259eb8fd4d8e520144ba93720b63fcfe9d1
Stefan Koch <stefkoch@de.ibm.com>
2016-04-16 01:03:50 +0200

    npm bootstrap

--- module.js
+++ module.js
@@ -5,9 +5,32 @@
 
 module.exports = function(RED) {
 
+  var aio = require('asterisk.io'),
+    agi = null;
+
   function AsteriskControllerNode(config) {
     console.log("AsteriskControllerNode", config);
     RED.nodes.createNode(this, config);
+
+    console.log("start service");
+
+    agi = aio.agi(14000); // port and host
+    // if host is missing then
+    // '0.0.0.0' is used as host
+    agi.on('error', function(err) {
+      throw err;
+    });
+
+    agi.on('listening', function() {
+      console.log('listening on port 14000');
+    });
+
+    agi.on('close', function() {
+      console.log('close');
+    });
+
+    console.log("service started");
+
 
   }
   RED.nodes.registerType("asterisk-controller", AsteriskControllerNode);
